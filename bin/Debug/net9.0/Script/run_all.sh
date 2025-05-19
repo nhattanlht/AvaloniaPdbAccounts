5f25570e2888b4a28abb55fca4189095ed7358a0
Jake Pham <jakeptnn@gmail.com>
2025-05-19 14:34:44 +0700

    auto run script

--- bin/Debug/net9.0/Script/run_all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# run_all.sh
-export NLS_LANG=AMERICAN_AMERICA.AL32UTF8
-
-# 1. Chạy file SQL
-sqlplus AdminPdb/123@localhost:1521/PDB @database.sql
-
-# 2. Load dữ liệu bằng SQL*Loader
-# sqlldr AdminPdb/123@localhost:1521/PDB control=sinhvien.ctl log=data_sinhvien.log bad=sinhvien.bad
